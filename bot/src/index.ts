fcf8384a082f634a7c493eaf02b308659436b047
Markus Wedler <markuswedler8@gmail.com>
2024-06-01 13:57:09 +1000

    fix: cors

--- bot/src/index.ts
+++ bot/src/index.ts
@@ -40,10 +40,11 @@ bot.onText(/\/id/, (msg) => {
 
 const app = express()
 app.use(express.json())
-app.use((req, res) => {
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Methods", "GET, POST")
   res.header("Access-Control-Allow-Headers", "Content-Type")
+  next()
 })
 
 app.get("/", (req, res) => {
