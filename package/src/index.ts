0509ba7da2e292ae014eed8a849ce3d2cd5a9601
Markus Wedler <markuswedler8@gmail.com>
2024-06-04 18:54:28 +1000

    refactor & return with generic for function scope catcher

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,21 +1,12 @@
-type Function = (...args: any[]) => Promise<any> | any
-type Options = { log: boolean; crash: boolean }
+type Function<T = unknown> = (...args: any[]) => Promise<T> | T
 
-type SendConfig = { userId: number }
-type TgErrorSender = (error: unknown, config: SendConfig) => Promise<Response>
-type TgFunctionScopeCatcher = (fn: Function, config: SendConfig, options?: Options) => Promise<void>
-
-type GlobalSendConfigEnable = { enable: true; userId: number }
-type GlobalSendConfigDisable = { enable: false }
-type TgGlobalScopeCatcher = (
-  config: GlobalSendConfigEnable | GlobalSendConfigDisable,
-  options?: Options
-) => Promise<void>
+export default { sendError, functionScopeCatcher, globalScopeCatcher }
 
 /* Error sender */
 
-export const sendError: TgErrorSender = async (error, { userId }) =>
-  await fetch("https://telegramreports.onrender.com", {
+export function sendError(error: unknown, config: { userId: number }) {
+  const { userId } = config
+  return fetch("https://telegramreports.onrender.com", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
@@ -23,27 +14,31 @@ export const sendError: TgErrorSender = async (error, { userId }) =>
       text: "```\n" + (error instanceof Error ? error.stack ?? error : error) + "\n```",
     }),
   })
+}
 
 /* Function scope */
 
-export const functionScopeCatcher: TgFunctionScopeCatcher = async (
-  fn,
-  { userId },
-  options = { log: true, crash: true }
-) => {
+export async function functionScopeCatcher<T = unknown>(
+  fn: Function<T>,
+  config: { userId: number },
+  options: { log: boolean; crash: boolean } = { log: true, crash: true }
+) {
   const { log, crash } = options
   try {
-    await fn()
+    return await fn()
   } catch (error) {
     if (log) console.error(error)
-    await sendError(error, { userId })
+    await sendError(error, config)
     if (crash) process.exit(1)
   }
 }
 
 /* Global scope */
 
-const globalError = async ({ error, userId }: { error: unknown; userId: number }, { log, crash }: Options) => {
+const globalError = async (
+  { error, userId }: { error: unknown; userId: number },
+  { log, crash }: { log: boolean; crash: boolean }
+) => {
   if (log) console.error(error)
   await sendError(error, { userId })
   if (crash) process.exit(1)
@@ -52,25 +47,20 @@ const globalError = async ({ error, userId }: { error: unknown; userId: number }
 let unhandledRejectionHandler: (error: unknown) => Promise<void>
 let uncaughtExceptionHandler: (error: unknown) => Promise<void>
 
-export const globalScopeCatcher: TgGlobalScopeCatcher = async (config, options = { log: true, crash: true }) => {
-  if (config.enable) {
-    if (!config.userId) throw new Error("userId is required")
-    const { userId } = config
-    unhandledRejectionHandler = (error) => globalError({ error, userId }, options)
-    uncaughtExceptionHandler = (error) => globalError({ error, userId }, options)
-    process.on("unhandledRejection", unhandledRejectionHandler)
-    process.on("uncaughtException", uncaughtExceptionHandler)
-  } else {
-    process.removeListener("unhandledRejection", unhandledRejectionHandler)
-    process.removeListener("uncaughtException", uncaughtExceptionHandler)
-  }
+export async function globalScopeCatcher(
+  config: { userId: number },
+  options: { log: boolean; crash: boolean } = { log: true, crash: true }
+) {
+  const { userId } = config
+  unhandledRejectionHandler = (error) => globalError({ error, userId }, options)
+  uncaughtExceptionHandler = (error) => globalError({ error, userId }, options)
+  process.on("unhandledRejection", unhandledRejectionHandler)
+  process.on("uncaughtException", uncaughtExceptionHandler)
 }
-
-export default { sendError, functionScopeCatcher, globalScopeCatcher }
 
 /* Test zone */
 
-const userId = 1419377014
+// const userId = 1419377014
 
 // // error sender
 // sendError(new Error("error sender"), { userId })
@@ -79,7 +69,7 @@ const userId = 1419377014
 // const fun = () => {
 //   throw new Error("function scope error")
 // }
-// functionScopeCatcher(fun, { userId }, { log: true, crash: false })
+// functionScopeCatcher<string>(fun, { userId }, { log: true, crash: false })
 
 // // async function
 // const asyncFun = async () => {
@@ -88,6 +78,4 @@ const userId = 1419377014
 // functionScopeCatcher(asyncFun, { userId }, { log: true, crash: false })
 
 // // global
-// globalScopeCatcher({ enable: true, userId }, { log: true, crash: false })
-// globalScopeCatcher({ enable: false })
-// throw new Error("global scope error")
+// globalScopeCatcher({ userId }, { log: true, crash: false })
